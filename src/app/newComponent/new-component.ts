5db67f1bd8a3dbe8a359ca9c0c299d01ba269408
Melissa Gomes <mel.ge171201@gmail.com>
2021-07-04 21:16:53 -0300

    criando um projeto angular, criando um novo componente e aprendendo a como renderiza-lo na tela com pouco estilizaçao para test

---
+++ src/app/newComponent/new-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-new-component',
+  templateUrl: './new-component.html',
+  styleUrls: ['./new-component.scss']
+})
+
+export class newComponente {
+
+}
